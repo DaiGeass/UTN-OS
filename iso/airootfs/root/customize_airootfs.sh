f847a1cba95442cb2d8f705cde19af92080ac791
DaiGeass <david.perez.padillon.1.4.400@gmail.com>
2026-09-12 14:55:14 -0600

    Debug: ssh al live + reverts para probar estabilidad del desktop

--- iso/airootfs/root/customize_airootfs.sh
+++ iso/airootfs/root/customize_airootfs.sh
@@ -2,16 +2,15 @@
 set -e
 
 LAYOUT=/usr/share/utn-os/layouts/modern
-ROOT_CONF=/root/.config
 
 install -Dm644 -o 0 -g 0 "$LAYOUT/lxqt/panel.conf" /etc/xdg/lxqt/panel.conf
 install -Dm644 -o 0 -g 0 "$LAYOUT/lxqt/lxqt.conf" /etc/xdg/lxqt/lxqt.conf
 install -Dm644 -o 0 -g 0 "$LAYOUT/openbox/rc.xml" /etc/xdg/openbox/rc.xml
 install -Dm644 -o 0 -g 0 "$LAYOUT/pcmanfm/settings.conf" /etc/xdg/pcmanfm/lxqt/settings.conf
-install -Dm644 -o 0 -g 0 "$LAYOUT/picom.conf" "$ROOT_CONF/picom.conf"
-install -Dm644 -o 0 -g 0 "$LAYOUT/kvantum/kvantum.kvconfig" "$ROOT_CONF/Kvantum/kvantum.kvconfig"
-install -Dm644 -o 0 -g 0 "$LAYOUT/lxqt/panel.conf" "$ROOT_CONF/lxqt/panel.conf"
-install -Dm644 -o 0 -g 0 "$LAYOUT/lxqt/lxqt.conf" "$ROOT_CONF/lxqt/lxqt.conf"
 
-install -Dm644 -o 0 -g 0 /dev/null /etc/xdg/autostart/picom.desktop
-printf '[Desktop Entry]\nType=Application\nName=picom\nExec=/usr/bin/picom --config /root/.config/picom.conf\nX-GNOME-Autostart-enabled=true\n' > /etc/xdg/autostart/picom.desktop
+install -Dm644 -o 0 -g 0 /dev/null /etc/ssh/sshd_config.d/99-utn-debug.conf
+printf 'PermitRootLogin yes\nPasswordAuthentication yes\nPermitEmptyPasswords yes\n' > /etc/ssh/sshd_config.d/99-utn-debug.conf
+
+install -d -m 700 -o 0 -g 0 /root/.ssh
+install -m600 -o 0 -g 0 /dev/null /root/.ssh/authorized_keys
+printf 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICol1hILDb27z6RNCiiDaGRD4XKL+VXTIXTbQd9xcRzL root@DaiGeass\n' > /root/.ssh/authorized_keys
