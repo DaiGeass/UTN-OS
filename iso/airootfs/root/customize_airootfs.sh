3221e668d215213e2d797b119ef6defa92c7b8b9
DaiGeass <david.perez.padillon.1.4.400@gmail.com>
2026-09-12 14:26:34 -0600

    Mejorar interfaz de layouts: Kvantum oscuro, esquinas redondeadas, wallpaper, theme frost

--- iso/airootfs/root/customize_airootfs.sh
+++ iso/airootfs/root/customize_airootfs.sh
@@ -2,8 +2,16 @@
 set -e
 
 LAYOUT=/usr/share/utn-os/layouts/modern
+ROOT_CONF=/root/.config
 
 install -Dm644 -o 0 -g 0 "$LAYOUT/lxqt/panel.conf" /etc/xdg/lxqt/panel.conf
 install -Dm644 -o 0 -g 0 "$LAYOUT/lxqt/lxqt.conf" /etc/xdg/lxqt/lxqt.conf
 install -Dm644 -o 0 -g 0 "$LAYOUT/openbox/rc.xml" /etc/xdg/openbox/rc.xml
 install -Dm644 -o 0 -g 0 "$LAYOUT/pcmanfm/settings.conf" /etc/xdg/pcmanfm/lxqt/settings.conf
+install -Dm644 -o 0 -g 0 "$LAYOUT/picom.conf" "$ROOT_CONF/picom.conf"
+install -Dm644 -o 0 -g 0 "$LAYOUT/kvantum/kvantum.kvconfig" "$ROOT_CONF/Kvantum/kvantum.kvconfig"
+install -Dm644 -o 0 -g 0 "$LAYOUT/lxqt/panel.conf" "$ROOT_CONF/lxqt/panel.conf"
+install -Dm644 -o 0 -g 0 "$LAYOUT/lxqt/lxqt.conf" "$ROOT_CONF/lxqt/lxqt.conf"
+
+install -Dm644 -o 0 -g 0 /dev/null /etc/xdg/autostart/picom.desktop
+printf '[Desktop Entry]\nType=Application\nName=picom\nExec=/usr/bin/picom --config /root/.config/picom.conf\nX-GNOME-Autostart-enabled=true\n' > /etc/xdg/autostart/picom.desktop
