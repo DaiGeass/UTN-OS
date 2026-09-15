1e8ade5fe79d2bde304a8e28a25572862ce17af2
DaiGeass <david.perez.padillon.1.4.400@gmail.com>
2026-09-14 20:07:02 -0600

    Agregar instalador gráfico INSTALL UTNOS, layouts macos/windows8, welcome, splash de boot y compositor en el inicio

--- iso/airootfs/root/customize_airootfs.sh
+++ iso/airootfs/root/customize_airootfs.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-LAYOUT=/usr/share/utn-os/layouts/modern
+LAYOUT=/usr/share/utn-os/layouts/macos
 
 install -Dm644 -o 0 -g 0 "$LAYOUT/lxqt/panel.conf" /etc/xdg/lxqt/panel.conf
 install -Dm644 -o 0 -g 0 "$LAYOUT/lxqt/lxqt.conf" /etc/xdg/lxqt/lxqt.conf
 install -Dm644 -o 0 -g 0 "$LAYOUT/openbox/rc.xml" /etc/xdg/openbox/rc.xml
 install -Dm644 -o 0 -g 0 "$LAYOUT/pcmanfm/settings.conf" /etc/xdg/pcmanfm/lxqt/settings.conf
+install -Dm644 -o 0 -g 0 "$LAYOUT/picom.conf" /etc/xdg/picom.conf
 
 install -Dm644 -o 0 -g 0 /dev/null /etc/ssh/sshd_config.d/99-utn-debug.conf
 printf 'PermitRootLogin yes\nPasswordAuthentication yes\nPermitEmptyPasswords yes\n' > /etc/ssh/sshd_config.d/99-utn-debug.conf
--- iso/profiledef.sh
+++ iso/profiledef.sh
@@ -22,4 +22,8 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/utn-installer"]="0:0:755"
+  ["/usr/local/bin/utn-installer-live"]="0:0:755"
+  ["/usr/local/bin/utn-welcome"]="0:0:755"
+  ["/usr/local/bin/utn-picom"]="0:0:755"
 )
